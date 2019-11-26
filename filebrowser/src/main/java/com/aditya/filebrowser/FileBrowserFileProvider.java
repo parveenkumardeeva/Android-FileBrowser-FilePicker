af0ca3216728feee36cd79718fe2082f1e5ecc39
parveenkumaryadav <parveenkumardeeva@gmail.com>
2019-11-26 12:46:31 +0530

    Custom FileProvider introduced to avoid collisions

---
+++ filebrowser/src/main/java/com/aditya/filebrowser/FileBrowserFileProvider.java
@@ -0,0 +1,9 @@
+package com.aditya.filebrowser;
+
+import androidx.core.content.FileProvider;
+
+/**
+ * Created by parveenkumaryadav on 2019-11-26.
+ */
+public class FileBrowserFileProvider extends FileProvider {
+}
